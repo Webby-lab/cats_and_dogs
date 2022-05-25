aea427ef429be61a848a87d62612a25a5e82e5ae
Webby-lab <keviczkyne.kover.krisztina@gmail.com>
2022-05-25 18:30:58 +0200

    registration page added

--- doggy/src/main/java/com/doggy/controllers/DogController.java
+++ doggy/src/main/java/com/doggy/controllers/DogController.java
@@ -14,12 +14,14 @@ import java.util.List;
 @Controller
 public class DogController {
     Owner geza = new Owner("Géza");
+    Owner eszter = new Owner("Eszter");
     List<Dog> dogs = Arrays.asList(
             new Dog("Pityu", 3, "fiú", Arrays.asList(Allergy.GYÓGYSZER), geza),
-            new Dog("Piri", 13, "lány", Arrays.asList(Allergy.GYÓGYSZER, Allergy.TISZTÍTÓ_TERMÉKEK), geza)
+            new Dog("Piri", 13, "lány", Arrays.asList(Allergy.GYÓGYSZER, Allergy.TISZTÍTÓ_TERMÉKEK), geza),
+            new Dog("Piroska", 1, "lány", Arrays.asList(Allergy.BŐR, Allergy.NÖVÉNY), eszter),
+            new Dog("Lajos", 6, "fiú", Arrays.asList(Allergy.ÉLELMISZER, Allergy.NÖVÉNY), eszter),
+            new Dog("Katika", 11, "lány", Arrays.asList(Allergy.GYÓGYSZER, Allergy.NÖVÉNY, Allergy.ÉLELMISZER), geza)
     );
-//     Dog one = new Dog("Pityu", 3, "fiú", Arrays.asList(Allergy.GYÓGYSZER), geza);
-//     Dog two = new Dog("Piri", 13, "lány", Arrays.asList(Allergy.GYÓGYSZER, Allergy.TISZTÍTÓ_TERMÉKEK), geza);
 
 
     @GetMapping("/dogs")
@@ -34,6 +36,10 @@ public class DogController {
         Dog dog = dogs.stream().filter((Dog d)-> d.getId() == id).findFirst().get();
         model.addAttribute("dog", dog);
         return "dogsProfilePage";
+    }
+    @GetMapping("/registration")
+    public String renderRegistrationPage() {
+        return "registration";
     }
 }
 
